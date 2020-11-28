3a6671c501d13902716cbcae3c08fc64872cca7c
Mridul0501 <mridul0501@gmail.com>
2020-11-28 17:13:52 +0530

    Drop Pack function written but call in xml handler needs to be fixed

--- src/game/action/creatureAction/playerAction/DropPack.java
+++ src/game/action/creatureAction/playerAction/DropPack.java
@@ -2,18 +2,42 @@ package game.action.creatureAction.playerAction;
 
 import game.action.creatureAction.CreatureAction;
 import game.displayable.creature.Creature;
+import game.displayable.creature.Player;
+import game.action.Action;
+import game.display.ObjectDisplayGrid;
+import game.display.Char;
+import game.displayable.item.Item;
 
 public class DropPack extends CreatureAction {
     private String name;
-    private Creature owner;
+    private Player owner;
 
-    public DropPack(String _name, Creature _owner) {
+    public DropPack(String _name, Player _owner) {
         super(_owner);
         name = _name;
         owner = _owner;
         System.out.println("DropPack Name:" + _name + " Dropack owner:"+_owner);
     }
-    public void doaction(){
+    public void performAction(){
+        System.out.println("DROPPPPPPPPPPPPPPPPPPPPPPPACKKKKKKKKKKKKKKKKKK");
+        int x = this.owner.getPosX();
+        int y = this.owner.getPosY();
+        ObjectDisplayGrid displayGrid;
+        displayGrid = ObjectDisplayGrid.getObjectDisplayGrid(0,0,0,0);
+        int item_number = 0;
+        Item item = this.owner.removeFromInventory(item_number);
+        if (item == null) {
+            displayGrid.displayStringToTerminal("Info: No item to be found at position " + item_number, 0, displayGrid.getTotalHeight() - 1);
+        }
+        else {
+            item.setPosX(x);
+            item.setPosY(y);
 
+            displayGrid.displayStringToTerminal("Info: Item dropped " + item.getName() + " at (" + item.getPosX() + ", " + item.getPosY() + ")", 0, displayGrid.getTotalHeight() - 1);
+            displayGrid.removeObjectToDisplay(x, y);
+            displayGrid.addObjectToDisplay(new Char(item.getType()), x, y);
+            displayGrid.getMainPlayer().setCharStandingOn(displayGrid.getDisplayChar(x, y));
+            displayGrid.addObjectToDisplay(new Char('@'), x, y);
+        }
     }
 }
